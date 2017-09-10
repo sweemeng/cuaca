4bba680e6bfe87f57d5d84cec3c5b8cf216167f2
sweemeng <sweester@gmail.com>
2017-09-10 09:49:49 +0800

    Commit setup.py as API endpoint known to work

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 import os
 from setuptools import setup
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = "Cuaca",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Ng Swee Meng",
     author_email = "sweester@gmail.com",
     description = ("A python wrapper for Malaysian Weather Service API"),
     license = "BSD",
     url = "https://github.com/sweemeng/cuaca",
     packages=['cuaca'],
-    long_description="This is a Wrapper for the Malaysian Weather Service API",
+    long_description=read("README.md"),
     install_requires = [
         'requests',
     ]
